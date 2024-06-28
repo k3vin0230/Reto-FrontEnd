bad41c94c76f45ab4ae223cfdd4911f8492daa6e
k3vin0230 <kevintrujillo1883@gmail.com>
2024-06-28 16:13:43 -0600

    Funciones de login creadas

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
+import { LoginService } from '../servicios/login.service';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  username: string = '';
+  password: string = '';
+
+  constructor(public ServicioLogin: LoginService, private router: Router) { }
+
+  inicioSesion() {
+    this.ServicioLogin.login(this.username, this.password).subscribe({
+      next: (response) => {
+        localStorage.setItem('token', response.token);
+        localStorage.setItem('role', response.role);
+        if (response.role === 'admin') {
+          this.router.navigate(['/adm']);
+        } else {
+          this.router.navigate(['/user']); 
+        }
+        Swal.fire({
+          icon: 'success',
+          title: 'Has iniciado sesión',
+          text: 'Bienvenido',
+        });
+      },
+      error: (err) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Contraseña o correo incorrecto',
+          text: 'Intenta de nuevo',
+        });
+      }
+    });
+    }
 
 }
